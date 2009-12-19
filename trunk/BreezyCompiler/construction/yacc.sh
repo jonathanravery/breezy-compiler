e9c918e17efbd32a2a1351fa2d076655fa599576
jackz9999 <jackz9999@95900fb4-cd5e-11de-af9b-21f9cdd2bce9>
2009-12-19 21:29:20 +0000

    Added "throws Exception" to yyparse

--- trunk/BreezyCompiler/construction/yacc.sh
+++ trunk/BreezyCompiler/construction/yacc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./yacc.exe -J -Jextends=Yylex -Jnorun Breezy.yacc
-sed -i -e 's/int yyparse()/int yyparse() throws IOException/' Parser.java
+sed -i -e 's/int yyparse()/int yyparse() throws IOException,Exception/' Parser.java
 cp Parser.java ../src/src
