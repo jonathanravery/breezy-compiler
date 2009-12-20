2832ac726f999d72d9672298a67ffb04b736e97c
Leighton.minor <Leighton.minor@95900fb4-cd5e-11de-af9b-21f9cdd2bce9>
2009-12-20 22:11:06 +0000

    Updated compiler.

--- BreezyCompiler/Breezy/bin/wind.bat
+++ BreezyCompiler/Breezy/bin/wind.bat
@@ -1,16 +1,41 @@
 @echo off
 
+
 java -jar %BREEZY_HOME%\dist\BreezyCompiler.jar %1 
 
-xcopy BreezyProg.java %BREEZY_HOME%\dist\ /Y
 
+xcopy BreezyProg.java %BREEZY_HOME%\dist\ /Q /Y
+
+
+@echo on
+@echo _____
+@echo *****
+@echo Launching Java Compiler
+@echo *****
+@echo off
 javac -classpath %BREEZY_HOME%\dist %BREEZY_HOME%\dist\BreezyProg.java
 
+
+@echo on
+@echo _____
+@echo *****
+@echo Launching Your Program
+@echo ****
+@echo off
 java -cp %BREEZY_HOME%\dist BreezyProg
 
+
+@echo on
+@echo _____
+@echo *****
+@echo Program Complete
+@echo ****
+@echo off
 del %BREEZY_HOME%\dist\BreezyProg.java
 
+
 del %BREEZY_HOME%\dist\BreezyProg.class
+
 
 del BreezyProg.java
 
