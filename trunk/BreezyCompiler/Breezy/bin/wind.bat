ce4e56770bd3f9e22daf3cdfe254fdad3e5d75b3
Leighton.minor <Leighton.minor@95900fb4-cd5e-11de-af9b-21f9cdd2bce9>
2009-12-21 01:56:28 +0000

    Updated Aesthetics to wind.bat

--- trunk/BreezyCompiler/Breezy/bin/wind.bat
+++ trunk/BreezyCompiler/Breezy/bin/wind.bat
@@ -1,22 +1,41 @@
 @echo off
 
 
-java -jar %BREEZY_HOME%\dist\BreezyCompiler.jar %1 
+REM Call the Breezy Compiler
+@echo on
+@echo.
+@echo _____
+@echo *****
+@echo Launching Breezy Compiler
+@echo *****
+@echo.
+@echo off
+java -jar %BREEZY_HOME%\dist\BreezyCompiler.jar %1
 
 
-xcopy BreezyProg.java %BREEZY_HOME%\dist\ /Q /Y
+
+REM After compiling, copy the new 
+REM java file into the folder with 
+REM the right class filesso that 
+REM the program can run.
+if exist BreezyProg.java xcopy BreezyProg.java %BREEZY_HOME%\dist\ /Q /Y > %BREEZY_HOME%\log.txt
+if not exist %BREEZY_HOME%\dist\BreezyProg.java goto :cleanup
 
 
 @echo on
+@echo.
 @echo _____
 @echo *****
 @echo Launching Java Compiler
 @echo *****
 @echo off
-javac -classpath %BREEZY_HOME%\dist %BREEZY_HOME%\dist\BreezyProg.java
+javac -nowarn -classpath %BREEZY_HOME%\dist %BREEZY_HOME%\dist\BreezyProg.java"
+if errorlevel 1 goto :nocompile
+
 
 
 @echo on
+@echo.
 @echo _____
 @echo *****
 @echo Launching Your Program
@@ -25,19 +44,37 @@ javac -classpath %BREEZY_HOME%\dist %BREEZY_HOME%\dist\BreezyProg.java
 java -cp %BREEZY_HOME%\dist BreezyProg
 
 
+
 @echo on
+@echo.
 @echo _____
 @echo *****
 @echo Program Complete
 @echo ****
 @echo off
-del %BREEZY_HOME%\dist\BreezyProg.java
+goto :cleanup
 
 
-del %BREEZY_HOME%\dist\BreezyProg.class
+
+:nocompile
+@echo on
+@echo.
+@echo _____
+@echo *****
+@echo Sorry, your program didn't compile in Java.  Our apologies.
+@echo ****
+@echo off
 
 
-del BreezyProg.java
+
+:cleanup
+REM Clean-up generated files so they don't
+REM interfere with the next build
+If exist %BREEZY_HOME%\dist\BreezyProg.java  del %BREEZY_HOME%\dist\BreezyProg.java  > %Breezy_Home%\log.txt
+If exist %BREEZY_HOME%\dist\BreezyProg.class del %BREEZY_HOME%\dist\BreezyProg.class > %Breezy_Home%\log.txt
+IF exist oldBreezyProg.java                  del oldBreezyProg.java                  > %Breezy_Home%\log.txt
+IF exist BreezyProg.java                     ren BreezyProg.java oldBreezyProg.java
+@echo.
 
 
 
